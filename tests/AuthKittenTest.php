785a12b841ca56e056490d4b749392dc6bda12a2
ha1t <halt.feits@gmail.com>
2012-02-02 11:20:21 +0900

    Travisで5.2もチェック対象に、テスト微妙に増やした

--- tests/AuthKittenTest.php
+++ tests/AuthKittenTest.php
@@ -11,9 +11,15 @@ class Auth_KittenTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Auth_Kitten', get_class($kitten));
     }
 
-    public function testFail()
+    public function testInvalidVerifyString()
     {
         $kitten = new Auth_Kitten();
         $this->assertFalse($kitten->verify('test'));
+    }
+
+    public function testInvalidVerifyArray()
+    {
+        $kitten = new Auth_Kitten();
+        $this->assertFalse($kitten->verify(array()));
     }
 }
